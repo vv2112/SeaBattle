4863c69b4c501664093ad12043713e0295b2a7b9
user-pro <vv2112@yandex.ru>
2024-06-04 23:16:13 +0300

    Деградировал функцию, переписал под свой код.

--- step1.py
+++ step1.py
@@ -6,30 +6,26 @@
 import random
 
 boats = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
+pole1 = []
+pole2 = []
 
-pole1 = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-pole2 = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+def init_field(field):
+    for yy in range(10):
+        line = []
+        for xx in range(10):
+            line.append(0)
+        field.append(line)
+
+def reset_field(field):
+    for yy in range(len(field)):
+        for xx in range(len(field[yy])):
+            field[yy][xx] = 0
+
 
 def printpole(pole1, pole2):   # Печатает оба поля
+    #for i in range(10):
+    #    print()
     print('      А Б В Г Д Е Ж З И К             А Б В Г Д Е Ж З И К')
     print('    |--------------------           |--------------------')
     for y in range(0, len(pole1)):     #  заполняет цифры слева от поля
@@ -46,42 +42,60 @@ def printpole(pole1, pole2):   # Печатает оба поля
         print()
 
 
-def place_ship(field, width, height, x1, y1):       # Размещает корабль в указанном поле с указанными координатами.
-    for iy in range(y1 - 1, y1 + height + 1):
-        for ix in range (x1 - 1, x1 + width + 1):
-            if iy < 0 or iy >= 10 or ix < 0 or ix >= 10:    # Проверка на выход за границы поля
+# def place_ship(field, width, height, x1, y1):       # Размещает корабль в указанном поле с указанными координатами.
+#     for iy in range(y1 - 1, y1 + height + 1):
+#         for ix in range (x1 - 1, x1 + width + 1):
+#             if iy < 0 or iy >= 10 or ix < 0 or ix >= 10:    # Проверка на выход за границы поля
+#                 continue
+#             if field[iy][ix] != 0:      # Проверка на то, что клетки размещения корабля и на 1 клетку вокруг не заняты.
+#                 return False
+#     for iy in range(y1, y1 + height):              # Размещение "корабля" на поле
+#         for ix in range (x1, x1 + width):
+#             field[iy][ix] = 2
+#     return True
+def place_ship(field, ship, horizont, x1, y1):
+    h = ship if not horizont else 1
+    w = ship if horizont else 1
+    for iy in range(y1 - 1, y1 + h + 1):
+        for ix in range(x1 - 1, x1 + w + 1):
+            if iy < 0 or iy >= 10 or ix < 0 or ix >= 10:
                 continue
-            if field[iy][ix] != 0:      # Проверка на то, что клетки размещения корабля и на 1 клетку вокруг не заняты.
+            if field[iy][ix] != 0:
                 return False
-    for iy in range(y1, y1 + height):              # Размещение "корабля" на поле
-        for ix in range (x1, x1 + width):
-            field[iy][ix] = 2
+    for i in range(ship):
+        if horizont: field[y1][x1+i] = 2                                          # три варианта решения
+        else: field[y1+i][x1] = 2
+
+        # field[y1+(i if not horizont else 0)][x1 + (i if horizont else 0)] = 2    # второй вариант
+        # field[y1+(i, 0)[horizont]][x1 + (0, i)[horizont]] = 2                      # третий вариант
+
     return True
 
-def place_ship_rnd(field, width, height):          # Генерация координаты случайного размещения корабля на поле
+def place_ship_rnd(field, boat):          # Генерация координаты случайного размещения корабля на поле
     while True:
-        x1 = random.randint(0, 10 - width)
-        y1 = random.randint(0, 10 - height)
-        is_placed = place_ship(field, width, height, x1, y1)    # вызов процедуры размещения корабля
+        horizont = random.randint(0, 1)
+        if horizont:
+            x1 = random.randint(0, 10 - boat)
+            y1 = random.randint(0, 10 - 1)
+        else:
+            x1 = random.randint(0, 10 - 1)
+            y1 = random.randint(0, 10 - boat)
+        is_placed = place_ship(field, boat, horizont, x1, y1)    # вызов процедуры размещения корабля
         if is_placed:                            # Если результат вызова процедуры True, то разрываем бесконечный цикл.
            break                                 #TODO А если не смогли разместить, то что???
 
 
 def autofill(field):
     for boat in boats:
-        if boat == 1:
-            place_ship_rnd(field, 1, 1)
-        else:
-            horizont = random.randint(0,1)                    # Генерация случайного положения горизонт/вертикаль
-            if horizont:
-                place_ship_rnd(field, boat, 1)
-            else:
-                place_ship_rnd(field, 1, boat)
+        place_ship_rnd(field, boat)
 
 
 
 
 if __name__ == '__main__':
+    init_field(pole1)
+    init_field(pole2)
     autofill(pole1)
-    autofill(pole2)
+    printpole(pole1, pole2)
+    reset_field(pole1)
     printpole(pole1, pole2)
--- test_step1.py
+++ test_step1.py
@@ -1,11 +1,13 @@
 import unittest
 from unittest import TestCase
 
-from step1 import printpole, autofill, pole1, pole2, place_ship
+from step1 import printpole, autofill, pole1, pole2, place_ship, init_field
 
 
 class MyTestCase(unittest.TestCase):
     def test_autofill(self):
+        init_field(pole1)
+        init_field(pole2)
         autofill(pole1)
         printpole(pole1, pole2)
         total = 0
@@ -15,16 +17,13 @@ class MyTestCase(unittest.TestCase):
                     total += 1
         self.assertEqual(4 + 3 * 2 + 2 * 3 + 4, total)
 
-
-#        self.assertEqual(True, False)  # add assertion here
+    def test_place_ship(self):
+        init_field(pole1)
+        init_field(pole2)
+        self.assertTrue(place_ship(pole1, 3, 1, 0, 0))
+        self.assertFalse(place_ship(pole1, 3, 1, 0, 0))
+        printpole(pole1, pole2)
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-class Test(TestCase):
-    def test_place_ship(self):
-        self.assertTrue(place_ship(pole1, 3, 1, 0, 0))
-        self.assertFalse(place_ship(pole1, 3, 1, 0, 0))
-        printpole(pole1, pole2)
